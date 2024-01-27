8a1f21e439b3e03b434a2d0b3ca986c96548b5b4
Imran Farooq <imran_farooq7@hotmail.com>
2024-01-27 23:46:01 +0500

    Create new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+
+const NewIssuePage = () => {
+	return (
+		<div className="max-w-xl p-5 space-y-3">
+			<TextField.Root>
+				<TextField.Input placeholder="Title" />
+			</TextField.Root>
+			<TextArea placeholder="Description" />
+			<Button radius="large">Submit New Issue</Button>
+		</div>
+	);
+};
+export default NewIssuePage;
