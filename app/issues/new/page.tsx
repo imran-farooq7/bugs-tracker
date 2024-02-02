d5cfa15e5165dad804e887c7f5678a7884a0f685
Imran Farooq <imran_farooq7@hotmail.com>
2024-02-03 01:34:19 +0500

    Form validation added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Text, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -7,7 +7,8 @@ import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 
 const NewIssuePage = () => {
-	const { register, handleSubmit, control } = useForm();
+	const { register, handleSubmit, control, formState } = useForm();
+	const { errors } = formState;
 	const router = useRouter();
 	const onSubmit = async (data: any) => {
 		await axios.post("/api/issues", data);
@@ -16,15 +17,32 @@ const NewIssuePage = () => {
 	return (
 		<form className="max-w-xl p-5 space-y-3" onSubmit={handleSubmit(onSubmit)}>
 			<TextField.Root>
-				<TextField.Input placeholder="Title" {...register("title")} />
+				<TextField.Input
+					placeholder="Title"
+					{...register("title", {
+						required: "Title is required",
+					})}
+				/>
 			</TextField.Root>
+			{errors.title && (
+				<Text color="red" className="pt-4">
+					{errors.title.message as string}
+				</Text>
+			)}
 			<Controller
 				render={({ field }) => (
 					<SimpleMDE placeholder="Description" {...field} />
 				)}
-				{...register("description")}
+				{...register("description", {
+					required: "Description is required",
+				})}
 				control={control}
 			/>
+			{errors.description && (
+				<Text color="red" className="block">
+					{errors.description.message as string}
+				</Text>
+			)}
 			<Button radius="large">Submit New Issue</Button>
 		</form>
 	);
